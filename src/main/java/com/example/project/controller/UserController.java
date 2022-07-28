5dbfd78e0278d16190333dc94e082c7bf577ec1b
Ravendra07 <kumarravendra055@gmail.com>
2022-07-28 12:56:25 +0530

    second commit

--- src/main/java/com/example/project/controller/UserController.java
+++ src/main/java/com/example/project/controller/UserController.java
@@ -1,18 +1,31 @@
 package com.example.project.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.project.dao.UserRepository;
+import com.example.project.entity.User;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	UserRepository userRepository;
 	
 	@GetMapping("/dashboard")
-	public String dashboard(Model model) {
-		model.addAttribute("title","User Dashboard" );
+	public String dashboard(Model model,Principal pricipal) {
 		
+		model.addAttribute("title","User Dashboard" );
+		String userName=pricipal.getName();
+		
+		User user=userRepository.getUserByUserName(userName);
+		model.addAttribute("user", user);
+		System.out.println(user);
 		return "normal/user_dashboard";
 	}
 }
